8027cf08b147c0e4d3f00e3698945e7bd7deba53
Sayeeda Waris <106420051+sayeedawaris@users.noreply.github.com>
2022-09-19 21:11:49 +0530

    Create post increment,decrement.c

---
+++ post increment,decrement.c
@@ -0,0 +1,22 @@
+//pre and post increment/decrement
+#include <stdio.h>
+
+int main() {
+
+  
+  int i=4, a=3;
+  
+  printf("%d\n",a);
+  printf("%d\n",a++);
+  printf("%d\n",a);
+  printf("%d\n",++a);
+  
+  printf("\n");
+  
+  printf("%d\n",i);
+  printf("%d\n",i--);
+  printf("%d\n",i);
+  printf("%d\n",--i);
+  
+  return 0;
+}
