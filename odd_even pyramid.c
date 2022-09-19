26654b1ac917d922a4af6b2d37d9bd4c584e3679
Sayeeda Waris <106420051+sayeedawaris@users.noreply.github.com>
2022-09-19 21:13:25 +0530

    Create odd_even pyramid.c

---
+++ odd_even pyramid.c
@@ -0,0 +1,31 @@
+/* print the following pattern
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+* * * * * * 
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j, k;
+    for(i=1;i<=6;i++)
+    {
+        for(j=6;j>i;j--)
+        { 
+            
+            printf(" ");
+        }
+       for(k=1;k<=i;k++)
+        {
+             printf("* ");
+              
+        }
+         printf("\n");   
+    }
+    
+    
+    return 0;
+}
