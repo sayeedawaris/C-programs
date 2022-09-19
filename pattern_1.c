2cff0b47666f30244e04015a636d24dbe92d2a9c
Sayeeda Waris <106420051+sayeedawaris@users.noreply.github.com>
2022-09-19 21:12:43 +0530

    Create pattern_1.c

---
+++ pattern_1.c
@@ -0,0 +1,21 @@
+/* print following pattern:
+*	*	*	*	
+*	*	*	*	
+*	*	*	*	
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    for(i=1;i<=3;i++)
+    {
+        for(j=1;j<=4;j++)
+        { 
+            printf("*\t");
+        }
+         printf("\n");   
+    }
+    
+    
+    return 0;
+}
