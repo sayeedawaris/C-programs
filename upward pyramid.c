3e7aa9cf75133309b30f008fae1de77ffec974ce
Sayeeda Waris <106420051+sayeedawaris@users.noreply.github.com>
2022-09-19 20:46:41 +0530

    Create upward pyramid.c

---
+++ upward pyramid.c
@@ -0,0 +1,24 @@
+/* print following pattern:
+*	
+*	*	
+*	*	*	
+*	*	*	*	
+*	*	*	*	*	
+*	*	*	*	*	*		
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    for(i=1;i<=6;i++)
+    {
+        for(j=1;j<=i;j++)
+        { 
+            printf("*\t");
+        }
+         printf("\n");   
+    }
+    
+    
+    return 0;
+}
